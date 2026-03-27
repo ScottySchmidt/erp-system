873d1ff639a51f0528042166b58bee3ee90bee3a
Balthild <ibalthild@gmail.com>
2026-03-26 23:54:24 -0500

    refactor: format code

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,23 +10,11 @@ import appCss from "#/styles.css?url";
 export const Route = createRootRouteWithContext<RouterContext>()({
   head: () => ({
     meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "TanStack Start Starter",
-      },
+      { charSet: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { title: "TanStack Start Starter" },
     ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
+    links: [{ rel: "stylesheet", href: appCss }],
   }),
   shellComponent: RootDocument,
   notFoundComponent: NotFoundPage,
